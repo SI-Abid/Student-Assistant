32cc9b027cdd87835e961b0d602da1499901e446
saiham <siaabid5323@gmail.com>
2022-01-22 17:32:53 +0600

    custom color added

---
+++ src/utils/Color.java
@@ -0,0 +1,42 @@
+package utils;
+
+public class Color extends java.awt.Color {
+    
+    public static final Color RED = new Color(255, 0, 0);
+    public static final Color GREEN = new Color(0, 255, 0);
+    public static final Color BLUE = new Color(0, 0, 255);
+    public static final Color BLACK = new Color(0, 0, 0);
+    public static final Color WHITE = new Color(255, 255, 255);
+    public static final Color CYAN = new Color(0, 255, 255);
+    public static final Color MAGENTA = new Color(255, 0, 255);
+    public static final Color YELLOW = new Color(255, 255, 0);
+    public static final Color LIGHT_GRAY = new Color(200, 200, 200);
+    public static final Color DARK_GRAY = new Color(100, 100, 100);
+    public static final Color LIGHT_PURPLE = new Color(115, 80, 200);
+    public static final Color DARK_PURPLE = new Color(128, 0, 128);
+    public static final Color LIGHT_GREEN = new Color(10, 150, 15);
+    public static final Color DARK_GREEN = new Color(0, 128, 0);
+    public static final Color LIGHT_BLUE = new Color(0, 0, 255);
+    public static final Color DARK_BLUE = new Color(0, 0, 128);
+    public static final Color LIGHT_RED = new Color(255, 0, 0);
+    public static final Color DARK_RED = new Color(128, 0, 0);
+    public static final Color LIGHT_ORANGE = new Color(255, 128, 0);
+    public static final Color DARK_ORANGE = new Color(128, 64, 0);
+    public static final Color LIGHT_YELLOW = new Color(255, 255, 0);
+    public static final Color DARK_YELLOW = new Color(128, 128, 0);
+    public static final Color LIGHT_PINK = new Color(255, 0, 255);
+    public static final Color DARK_PINK = new Color(128, 0, 128);
+    public static final Color LIGHT_CYAN = new Color(0, 255, 255);
+    public static final Color DARK_CYAN = new Color(0, 128, 128);
+    public static final Color LIGHT_BROWN = new Color(128, 64, 0);
+    public static final Color DARK_BROWN = new Color(64, 32, 0);
+    public static final Color LIGHT_GOLD = new Color(255, 255, 128);
+    public static final Color DARK_GOLD = new Color(128, 128, 64);
+    public static final Color LIGHT_VIOLET = new Color(255, 128, 255);
+    public static final Color DARK_VIOLET = new Color(128, 64, 128);
+
+    public Color(int r, int g, int b) {
+        super(r, g, b);
+    }
+
+}
